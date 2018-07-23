e3e4ff7baf2c3595934cf6e83f84f4a2c6d79c9b
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-23 11:30:36 -0600

    done for today

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
+import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
     render() {
@@ -11,7 +12,7 @@ class NewsletterGrid extends Component {
                 {/* add button */}
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                {/* content */}
+                <NewsletterLatest/>
             </div>
         )
     }
