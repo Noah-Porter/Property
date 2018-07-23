3afee7cb9dc3dac139e84dd2a5af6443e4797e52
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-23 09:06:08 -0600

    message

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -1,17 +1,12 @@
-
 import React, { Component } from 'react';
 
 class TabNav extends Component {
   render() {
-    var JSX = [
-        <h1>TABS</h1>
-    ];
+    return (
+        <div className='tab-nav'>
 
-    this.props.tabs.map((tab, index) => {
-        JSX.push(tab.component);
-    })
-
-    return JSX;
+        </div>
+    )
   }
 }
 
