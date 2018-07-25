a52d8ce96f3124489eb07469e0da48297c185af0
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-25 09:44:02 -0600

    finished week 6

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -59,3 +59,20 @@ export class FormButton extends Component {
         )
     }
 }
+
+export class FormImage extends Component {
+    render() {
+        const { className, title, input, type, imageUrl } = this.props;
+        return (
+            <div className={`${className} form-image`}>
+                <label className='form-image__title'>{title}</label>
+                <img
+                    className='form-image__image'
+                    src={imageUrl} 
+                />
+                <input {...input} type={type}/>
+                {/* replace button/input goes here */}
+            </div>
+        )
+    }
+}
